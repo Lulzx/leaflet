1041fe403fbd2eca804135b0619fb9371532aab5
Lulzx <wiserishabh@gmail.com>
2020-07-25 18:25:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+import os
 import sys
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters)
 from telegram import ParseMode
@@ -11,45 +12,60 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
+
 def start(update, context):
-	update.message.reply_text("Hi, Use /j, /red, /mathematica, /nim or /crystal followed with your code to run it.")
+    update.message.reply_text("Hi, Use /j, /red, /mathematica, /nim or /crystal followed with your code to run it.")
+
 
 def run(update, context):
-	text = update.message.text
-	if text.startswith('/'):
-		command = text.split(' ')[0]
-		lang = command[1:]
-		if lang not in ['crystal', 'nim', 'j', 'mathematica', 'red']:
-			return
-	code = text[len(command):]
-	request = TioRequest(lang=lang, code="")
-	if lang == 'j':
-		set_code = 'echo' + code
-	else:
-		set_code = code
-	request.set_code(set_code)
-	tio = Tio()
-	data = tio.send(request)
-	if data.error:
-		output = data.error
-	else:
-		output = data.result
-	update.message.reply_text("*[{}]*\n*Input*: `{}`".format(lang.upper(), str(code)) + "\n\n*Output*: `{}`".format(output), parse_mode=ParseMode.MARKDOWN)
-		
+    # global command, lang
+    text = update.message.text
+    if text.startswith('/'):
+        command = text.split(' ')[0]
+        lang = command[1:]
+        if lang not in ['crystal', 'nim', 'j', 'mathematica', 'red']:
+            return
+    try:
+    	code = text[len(command):]
+    except:
+    	return
+    request = TioRequest(lang=lang, code="")
+    if lang == 'j':
+        set_code = 'echo' + code
+    elif lang == 'red':
+        set_code = f"""
+        Red []
+        {code}
+        """
+    else:
+        set_code = code
+    request.set_code(set_code)
+    tio = Tio()
+    data = tio.send(request)
+    if data.error:
+        output = data.error
+    else:
+        output = data.result
+    update.message.reply_text(
+        "*[{}]*\n*Input*: `{}`".format(lang.upper(), str(code)) + "\n\n*Output*: `{}`".format(output),
+        parse_mode=ParseMode.MARKDOWN)
+
+
 def main():
-	try:
-		TOKEN = sys.argv[1]
-	except IndexError:
-		TOKEN = os.environ.get("TOKEN")
-	updater = Updater(TOKEN, use_context=True)
-	dp = updater.dispatcher
-	dp = updater.dispatcher
-	print("Ready to rock..!")
-	dp.add_handler(CommandHandler('start', start))
-	dp.add_handler(MessageHandler(Filters.text, run))
-	
-	updater.start_polling()
-	updater.idle()
+    try:
+        TOKEN = sys.argv[1]
+    except IndexError:
+        TOKEN = os.environ.get("TOKEN")
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp = updater.dispatcher
+    print("Ready to rock..!")
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(MessageHandler(Filters.text, run))
+
+    updater.start_polling()
+    updater.idle()
+
 
 if __name__ == '__main__':
-	main()
+    main()
